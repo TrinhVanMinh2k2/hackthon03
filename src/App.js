90026478f2347f089fabe06864cf425a57945e75
TrinhVanMinh2k2 <huyetcongtu2002@gmail.com>
2023-11-07 17:14:44 +0700

    hackthon03

--- src/App.js
+++ src/App.js
@@ -1,25 +1,55 @@
-import logo from './logo.svg';
+import React, { useState } from "react"
 import './App.css';
 
-function App() {
+
+function WordCounter() {
+
+  //them value tu input
+
+  const [text, setText] = useState("");
+  const handleChangeText = (e) => {
+    setText(e.target.value);
+  };
+
+  //in hoa + in thuong
+  const handleUp = () => {
+    setText(text.toUpperCase())
+  };
+  const handleLow = () => {
+    setText(text.toLowerCase())
+  };
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
-    </div>
+    <>
+      <div className="container">
+        <h1>Word Counter</h1>
+        <div className="row">
+          <div className="col-4">
+            <p>Word</p>
+            <span>{text ? text.trim().split(/\s+/).length : 0}</span>
+          </div>
+          <div className="col-4">
+            <p>Letter</p>
+            <span>{text ? text.length : 0}</span>
+          </div>
+          <div className="col-4">
+            <p>Paragraph</p>
+            <span>0</span>
+          </div>
+        </div>
+        <div className="form">
+          <textarea
+            onChange={handleChangeText}
+            value={text}
+            className="text"
+          />
+        </div>
+        <div className="button">
+          <button className="btn" onClick={handleUp}>Click to Uppenrcase</button>
+          <button className="btn" onClick={handleLow}>Click to Lowercase</button>
+        </div>
+      </div>
+    </>
   );
 }
 
-export default App;
+export default WordCounter;
